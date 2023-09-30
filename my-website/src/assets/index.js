27a5e8c3a0665e41fa60c6664780f831b322af22
Kurt Higa <kaato@Kurts-MacBook-Pro.local>
2023-09-29 20:47:53 -0500

    start over

--- my-website/src/assets/index.js
+++
@@ -1,4 +0,0 @@
-export { default as sunsetGif }  from  './sunset.gif';
-export { default as treeGif}  from './tree.gif';
-export { default as oceanGif } from './ocean.gif';
-export { default as scene } from './scene.png'
--- my-website/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { MyBio } from './bio';
-export { PageBackground } from './background'
-export { MenuBar } from './menuBar';
