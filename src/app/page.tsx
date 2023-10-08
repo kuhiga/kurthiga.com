14b7b2b80b8439b6f143683d08b3447d57a603ae
Kurt Higa <kaato@Kurts-MacBook-Pro.local>
2023-10-08 00:51:41 -0500

    type fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ const Burning = () => {
       />
       <div className="flex justify-start pt-20 text-left align-middle w-full h-128">
         <div className="flex flex-col m-w-md gap-y-10">
-          <h1>Hey, I'm Kurt</h1>
+          <h1>Hey, I’m Kurt</h1>
           <h1>
-            I’m a full stack engineer at Asurion, specializing in building
-            mobile and web applications.
+            I’m a full stack product engineer at Asurion, specializing in
+            building mobile and web applications.
           </h1>
           <h1>
             Some tools I like to use are React, TypeScript, Node, and AWS.
